f2741b2a00ae0eaed0bf76fbec2c658deca48376
ajinkya khairnar <ajinkyakhairnar08@gmail.com>
2025-11-27 13:01:02 +0530

    update

--- javascript.js
+++ javascript.js
@@ -8,3 +8,47 @@ navLinks.forEach(link => {
     link.classList.add("active");
   });
 });
+
+function remove(){
+  let confermdelete = confirm("Are you sure you want to delete this post?");
+  if(confermdelete){
+   document.getElementById("postid").innerHTML = "";
+  }
+  else{
+    console.log(" not deleted");
+  }
+}
+function remove1(){
+  let confermdelete = confirm("Are you sure you want to delete this post?");
+  if(confermdelete){
+   document.getElementById("postid1").innerHTML = "";
+  }
+  else{
+    console.log(" not deleted");
+  }
+}
+function remove2(){
+  let confermdelete = confirm("Are you sure you want to delete this post?");
+  if(confermdelete){
+   document.getElementById("postid2").innerHTML = "";
+  }
+  else{
+    console.log(" not deleted");
+  }
+}
+function reels(){
+  let confirmReels = confirm("Go to reels section?");
+  if (confirmReels) {
+    window.location.href = "reel.html";
+  }
+  else{
+    window.location.href = "index.html";
+  }
+}
+document.querySelectorAll(".like-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("liked");
+  });
+});
+
+
